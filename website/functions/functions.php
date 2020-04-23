61298dbb4c00f3fbdf369a81a26d7bf8c0ad4f8b
yousefahmed98 <36170310+yousefahmed98@users.noreply.github.com>
2020-04-24 01:06:20 +0200

    getUserIP() and longitude and latitude is saved in cookies

--- website/functions/functions.php
+++ website/functions/functions.php
@@ -8,25 +8,46 @@
             echo 'Default';
         }
     }
-
+    //debug function
+    function debug($data) {
+        $output = $data;
+        if (is_array($output))
+            $output = implode(',', $output);
+    
+        echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+    }
     // get user IP
     function getUserIp(){
-        if (isset($_SERVER['HTTP_CLIENT_IP']))
-            $header = $_SERVER['HTTP_CLIENT_IP'];
-        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-            $header = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        else if(isset($_SERVER['HTTP_X_FORWARDED']))
-            $header = $_SERVER['HTTP_X_FORWARDED'];
-        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
-            $header = $_SERVER['HTTP_FORWARDED_FOR'];
-        else if(isset($_SERVER['HTTP_FORWARDED']))
-            $header = $_SERVER['HTTP_FORWARDED'];
-        else
-            $header = NULL;
-
-        $user_ip = $_SERVER['REMOTE_ADDR'];
-
-        return $user_ip;
+        //print("<script>console.log('getUserIp1');</script>");
+        if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+            //ip from share internet
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+            //ip pass from proxy
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }else{
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
     }
-    
+
+    // get user adress
+    function getAddress($latitude,$longitude){
+        if(!empty($latitude) && !empty($longitude)){
+            //Send request and receive json data by address
+            $queryString = http_build_query([
+                'access_key' => 'a7ceb1189a28d7b65a2134cb1d7fead9',
+                'query' => '29.9088201,31.274070199999997',
+                'language' => 'ES',
+              ]);
+              $ch = curl_init(sprintf('%s?%s', 'https://api.positionstack.com/v1/reverse', $queryString));
+              curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+              $json = curl_exec($ch);
+              
+              curl_close($ch);
+              
+              $apiResult = json_decode($json, true);
+              print_r($apiResult);
+    }
+} 
 ?>
