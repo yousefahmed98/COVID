0f8f3898303e501b6df68154073f455f58a6bda9
heba mohamed <36227122+hebaamohaamed@users.noreply.github.com>
2020-04-26 22:55:57 +0200

    insertion2

--- website/views/saveRegInfo.php
+++ website/views/saveRegInfo.php
@@ -5,9 +5,7 @@ $sickness=$_GET["sickness"];
 $username=$_POST["username"];
 $email=$_POST["email"];
 $phone=$_POST["phoneNumber"];
-/*echo $username;
-echo $email;
-echo $phone;*/
+
 
 $ip =  getUserIp();
 if($ip=="")
@@ -18,12 +16,10 @@ if($ip=="")
 $sick = $_GET["sick"];
 $date = get_Date();
 
-/*$result = mysql_query("SELECT MAX(`LastExamID`) FROM `tbl_users`");
+$result = mysql_query("SELECT MAX(`LastExamID`) FROM `tbl_users`");
 $row = mysql_fetch_row($result);
-$exam_id = $row[0]+1;*/
+$exam_id = $row[0]+1;
 
-
-$exam_id=1;
 
 $ip=getUserIp();
 if($ip=="")
@@ -31,11 +27,17 @@ if($ip=="")
 
 $sql="INSERT INTO `tbl_users`(`FullUsername`, `Email`, `Phone`, `IP`, `LastExamResult`, `LastExamID`, `TimeSt`) VALUES ('".$username."','".$email."','".$phone."','".$ip."','".$sick."','".$exam_id."','".$date."')";
 
+if(mysql_query( $sql, $db )){
+
+}else{
+    header("Location:checkDone.php? sickness = $sick");
+}
+/*
 if($db->exec($sql)){
    header("Location:index.php");
 }else{
     header("Location:checkDone.php");
 }
-
+*/
 
 ?>
